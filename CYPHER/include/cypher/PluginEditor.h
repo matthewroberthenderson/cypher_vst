8745d055badc28e43039c17fe4417d1dcb15a537
Matthew Henderson <32535159+matthewroberthenderson@users.noreply.github.com>
2025-01-02 12:24:35 +1100

    going back to basics

--- CYPHER/include/cypher/PluginEditor.h
+++ CYPHER/include/cypher/PluginEditor.h
@@ -1,86 +1,22 @@
-#ifndef PLUGIN_EDITOR_H
-#define PLUGIN_EDITOR_H
+#pragma once
 
 #include "PluginProcessor.h"
-#include "cypher/Cypher.h"
-#include "JuceHeader.h"
-namespace audio_plugin {
 
-class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor,
-    private juce::Timer,
-    private juce::Value::Listener {
-
+//==============================================================================
+class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
 public:
-  explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor&);
-  ~AudioPluginAudioProcessorEditor() override;
+    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+    ~AudioPluginAudioProcessorEditor() override;
 
-  std::unique_ptr<CypherRenderer> openGLComponent;
-
-  void paint(juce::Graphics&) override;
-  void resized() override;
-
-  MidiKeyboardComponent midiKeyboard;
-
-  Label gainLabel{ {}, "Throughput level:" },
-      delayLabel{ {}, "Delay:" },
-      EquationEditorLabel{ {}, "Equation Editor" };
-
-  Slider gainSlider, delaySlider;
-
-  void valueChanged(Value&) override
-  {
-      setSize(lastUIWidth.getValue(), lastUIHeight.getValue());
-  }
-
-  AudioProcessorValueTreeState::SliderAttachment gainAttachment, delayAttachment;
-
-  Colour backgroundColour = Colour();
-  Value lastUIWidth, lastUIHeight;
-
-  double custom_equation_value = 0;
-  EquationEditor equation_editor;
-  double math_value;
-  value_holder* value_holder_instance_ref;
-
-  void timerCallback() override;
-  void hostMIDIControllerIsAvailable(bool controllerIsAvailable) override
-  {
-      midiKeyboard.setVisible(!controllerIsAvailable);
-  }
-
-  int getControlParameterIndex(Component& control) override
-  {
-      if (&control == &gainSlider)
-          return 0;
-
-      if (&control == &delaySlider)
-          return 1;
-
-      return -1;
-  }
-
-
-  AudioPluginAudioProcessor& getProcessor() const
-  {
-      return static_cast<AudioPluginAudioProcessor&> (processor);
-  }
-
-  void updateTrackProperties()
-  {
-      auto trackColour = getProcessor().getTrackProperties().colour;
-      auto& lf = getLookAndFeel();
-
-      /* backgroundColour = (trackColour == Colour() ? lf.findColour (ResizableWindow::backgroundColourId)
-                                                   : trackColour.withAlpha (1.0f).withBrightness (0.266f));*/
-      repaint();
-  }
-
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
 
 private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    AudioPluginAudioProcessor& processorRef;
 
-
-
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
-}  // namespace audio_plugin
-#endif
--- CYPHER/source/OpenGL.cpp
+++
@@ -1,1 +0,0 @@
-#include <cypher/OpenGL.h>
